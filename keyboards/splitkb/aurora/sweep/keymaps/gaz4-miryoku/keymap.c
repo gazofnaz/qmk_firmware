61d5258763d9c697746f7377fb8eaebfc87ae19a
Gareth Arnott <gareth.arnott@thirdbridge.com>
2024-11-06 06:44:42 +0000

    intial number and symbol layers

--- keyboards/splitkb/aurora/sweep/keymaps/gaz4-miryoku/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/gaz4-miryoku/keymap.c
@@ -15,29 +15,29 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
 //   |----LEFT-------------------------------------------------------------------   |----RIGHT-----------------------------------------------------------------------------
-        KC_Q,  KC_W,  KC_E,               KC_R,                                         KC_T,    KC_Y,           KC_U,   KC_I,     KC_O,    KC_P,
-        LGUI_T(KC_A),  LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),       KC_G,                                          KC_H,    RSFT_T(KC_J),   RCTL_T(KC_K),     RALT_T(KC_L),    RGUI_T(KC_SCLN),
-        KC_Z,  KC_X,  KC_C,               KC_V,                    KC_B,                KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLASH,
-                                          LT(NAV, KC_SPC),    LT(MOU, KC_TAB),                          LT(SYM, KC_ENT),  LT(NUM, KC_BSPC)
+        KC_Q,          KC_W,          KC_E,             KC_R,             KC_T,       KC_Y,             KC_U,             KC_I,          KC_O,          KC_P,
+        LGUI_T(KC_A),  LALT_T(KC_S),  LCTL_T(KC_D),     LSFT_T(KC_F),     KC_G,       KC_H,             RSFT_T(KC_J),     RCTL_T(KC_K),  RALT_T(KC_L),  RGUI_T(KC_SCLN),
+        KC_Z,          KC_X,          KC_C,             KC_V,             KC_B,       KC_N,             KC_M,             KC_COMM,       KC_DOT,        KC_SLASH,
+                                      LT(NAV, KC_SPC),  LT(MOU, KC_TAB),              LT(SYM, KC_ENT),  LT(NUM, KC_BSPC)
 //   |----LEFT-------------------------------------------------------------------   |----RIGHT-----------------------------------------------------------------------------
   ),
 
   [SYM] = LAYOUT(
-//   |----LEFT-------------------------------------------------------------------------   |----RIGHT---------------------------------------------------------------
-        KC_NO, KC_NO,   KC_NO,   KC_NO,     KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
-        KC_NO, KC_NO,   KC_NO,   KC_NO,     KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
-        KC_NO, KC_NO,   KC_NO,   KC_NO,     KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
-                                 KC_NO,     KC_NO,              KC_NO,   KC_NO
-//   |----LEFT-------------------------------------------------------------------------   |----RIGHT-----------------------------------------------------------------
+//   |----LEFT-------------------------------------------------------------------   |----RIGHT---------------------------------------------------------------
+        KC_LCBR,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RCBR,      KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,
+        KC_COLN,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_PLUS,      KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,
+        KC_TILD,        KC_EXLM,        KC_AT,          KC_HASH,        KC_PIPE,      KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,
+                                                        KC_RPRN,        KC_UNDS,      KC_NO,         KC_NO
+//   |----LEFT-------------------------------------------------------------------   |----RIGHT-----------------------------------------------------------------
   ),
 
   [NUM] = LAYOUT(
-//   |----LEFT----------------------------------------------   |----RIGHT-------------------------------------
-        KC_NO, KC_NO,   KC_NO,   KC_NO,     KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
-        KC_NO, KC_NO,   KC_NO,   KC_NO,     KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
-        KC_NO, KC_NO,   KC_NO,   KC_NO,     KC_NO,              MO(FUN), KC_NO,   KC_NO,   KC_NO,  KC_NO,
-                                 KC_NO,     KC_NO,              KC_NO,   KC_NO
-//   |----LEFT----------------------------------------------   |----RIGHT-------------------------------------
+//   |----LEFT-------------------------------------------------------------------   |----RIGHT-----------------------------------------------------------------------
+        KC_LBRC,          KC_7,          KC_8,          KC_9,          KC_RBRC,        KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,
+        KC_SCLN,          KC_4,          KC_5,          KC_6,          KC_EQL,         KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,
+        KC_GRV,           KC_1,          KC_2,          KC_3,          KC_NUBS,        KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,
+                                                        KC_0,          KC_MINS,        KC_NO,         KC_NO
+//   |----LEFT-------------------------------------------------------------------   |----RIGHT-----------------------------------------------------------------------
   ),
 
   [NAV] = LAYOUT(
